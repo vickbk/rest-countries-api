a83a3e8e8db89b704ea7bd0329c0af79d48cef68
vickbk <vickbake8@gmail.com>
2025-12-06 22:22:30 +0200

    filter initial styling

--- app/components/main-page/filter.tsx
+++ app/components/main-page/filter.tsx
@@ -1,13 +1,19 @@
+import { Icon } from "../common/icon";
 import { SROnly } from "../shared/SROnly";
 
 export const Filter = () => {
   const regions = ["Africa", "America", "Asia", "Europe", "Oceania"];
   return (
-    <details>
-      <summary>Filter by Region</summary>
-      <form>
+    <details className="filter relative">
+      <summary className="filter__summary outstand">
+        Filter by Region{" "}
+        <span className=" ml-8">
+          <Icon name="chevron-down filter__icon" />
+        </span>
+      </summary>
+      <form className="outstand filter__list">
         {regions.map((region, key) => (
-          <label key={key}>
+          <label key={key} className="cursor-pointer">
             {region} <SROnly>region</SROnly>
             <input
               type="radio"
