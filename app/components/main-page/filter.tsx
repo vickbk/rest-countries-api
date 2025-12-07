d721e74c0c34917e0cadb601009db7429be27433
vickbk <vickbake8@gmail.com>
2025-12-08 00:45:35 +0200

    updating all continents

--- app/components/main-page/filter.tsx
+++ app/components/main-page/filter.tsx
@@ -2,19 +2,28 @@
 import { usePathname, useRouter } from "next/navigation";
 import { Icon } from "../common/icon";
 import { SROnly } from "../shared/SROnly";
-import { useDebouncedCallback } from "use-debounce";
 import { updateSearchParams } from "@/app/lib/update-search-params";
 import { useRef } from "react";
 
 export const Filter = () => {
-  const regions = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  const regions = [
+    "Africa",
+    "Asia",
+    "Europe",
+    "Oceania",
+    "Americas",
+    "Antarctic",
+  ];
   const pathname = usePathname();
   const { replace } = useRouter();
+
   const detailsElement = useRef<HTMLDetailsElement>(null);
-  const valueChange = useDebouncedCallback((value: string) => {
+
+  const valueChange = (value: string) => {
     replace(updateSearchParams({ param: "region", value, pathname }));
     detailsElement.current?.removeAttribute("open");
-  }, 300);
+  };
+
   return (
     <details className="filter relative" ref={detailsElement}>
       <summary className="filter__summary outstand">
