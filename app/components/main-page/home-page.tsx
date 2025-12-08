cf39d1fe6c2523b9838ceb308ca5311e8a796d78
vickbk <vickbake8@gmail.com>
2025-12-08 18:45:58 +0200

    fixing accessibility issue

--- app/components/main-page/home-page.tsx
+++ app/components/main-page/home-page.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from "react";
-import { SROnly } from "../shared/SROnly";
 import { CountriesPlaceHolder } from "./countries/country-placeholder";
 import { Filter } from "./filter";
 import { Search } from "./search";
@@ -19,7 +18,7 @@ export const HomePage = (props: {
         <Filter region={region} />
       </section>
       <section className="p-8 grid gap-8 sm:grid-cols-2 sm:px-0 md:grid-cols-3 lg:grid-cols-4 text-[.875rem]">
-        <SROnly>List of countries</SROnly>
+        <h2 className="sr-only">List of countries</h2>
 
         <Suspense
           key={country + region + page}
